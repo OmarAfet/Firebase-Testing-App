c392f4970f783495dc7398f3c5e032fbbd3ab188
OmarAfet <OmarAlenezy2004@gmail.com>
2023-08-09 10:43:48 +0300

    Fix images path

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -12,7 +12,7 @@ const Button = ({ onClick, isLoading, disabled, hidden, text, className }) => {
       className={`text-gray-100 px-4 py-1 cursor-pointer bg-gray-900 rounded-lg hover:ring focus:ring ring-offset-1 ring-gray-900 flex justify-center items-center ${className}`}
     >
       {isLoading ? (
-        <img className="h-6 w-6" src="/GIF/Loading White.gif" alt="Loading" />
+        <img className="h-6 w-6" src="./GIF/Loading White.gif" alt="Loading" />
       ) : (
         text
       )}
